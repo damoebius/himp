e7afe90fd4ff1e83a8641c17c50dd50d6441c270
damoebius <tintin33$>
2017-08-13 19:55:13 +0200

    add test.html load big jpg arraybuffer via XHR add bench log call Himp

--- src/Himp.hx
+++ src/Himp.hx
@@ -1,8 +1,39 @@
 package ;
 
-class Himp {
+import js.html.Uint8Array;
+import js.Error;
+import haxe.MimeType;
+import js.html.ImageData;
+
+@:expose @:keep class Himp {
+
+    private static var _instance:Himp;
+
+    private var _data:Uint8Array;
+    private var _mimeType:MimeType;
 
     public static function main( ):Void {
-        trace("coucu");
+        _instance = new Himp();
     }
+
+    public static function read(data:Uint8Array, mimeType:MimeType):Void{
+         _instance.process(data, mimeType);
+    }
+
+    private function new(){}
+
+    private function process(data:Uint8Array, mimeType:MimeType):Void{
+        _data = data;
+        _mimeType = mimeType;
+        switch (_mimeType) {
+            case MimeType.PNG:
+                trace("png");
+
+            case MimeType.JPEG:
+                trace("jpg");
+            default:
+                throw(new Error("unknown mime type"));
+        }
+    }
+
 }
